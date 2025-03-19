0718795dcd7a36422de0a136cf9ca173bb475572
gabilevit <gabilevit@gmail.com>
2025-03-19 21:50:09 +0200

    Added the state files and inmplemented basic transition between states

---
+++ Graphics/IdleState.cpp
@@ -0,0 +1,31 @@
+#include "IdleState.h"
+#include "SeekResourcesState.h"
+#include "Squire.h"
+#include <iostream>
+
+void IdleState::OnEnter(NPC* p)
+{
+    // Initialize idle behavior
+    Squire* s = (Squire*)p;
+    s->SetIsMoving(true);
+    std::cout << "Entering IdleState\n";
+}
+
+void IdleState::Transition(NPC* p)
+{
+    // Exiting from current state
+    OnExit(p);
+
+    Squire* s = (Squire*)p;
+    s->SetState(new SeekResourcesState());
+
+    // entering new state
+    p->GetState()->OnEnter(p);
+}
+
+void IdleState::OnExit(NPC* p)
+{
+    // Clean up idle behavior
+    p->SetIsMoving(true);
+    std::cout << "Exiting IdleState\n";
+}
---
+++ Graphics/SeekResourcesState.cpp
@@ -0,0 +1,31 @@
+#include "SeekResourcesState.h"
+#include "DeliverResourcesState.h"
+#include "Squire.h"
+#include <iostream>
+
+void SeekResourcesState::OnEnter(NPC* p)
+{
+    // Initialize seek resources behavior
+    Squire* s = (Squire*)p;
+    s->SetIsMoving(true);
+    std::cout << "Entering SeekResourcesState\n";
+}
+
+void SeekResourcesState::Transition(NPC* p)
+{
+    // Exiting from current state
+    OnExit(p);
+
+    Squire* s = (Squire*)p;
+    s->SetState(new DeliverResourcesState());
+
+    // entering new state
+    p->GetState()->OnEnter(p);
+}
+
+void SeekResourcesState::OnExit(NPC* p)
+{
+    // Clean up seek resources behavior
+	p->SetIsMoving(true);
+    std::cout << "Exiting SeekResourcesState\n";
+}
